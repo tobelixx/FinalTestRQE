fd6f603ad05ed1697e1052ccf9d1938c04adbe4b
Tobelix <tobias.schramek@gmx.at>
2020-11-05 10:22:43 +0100

    Projektdateien hinzufügen.

---
+++ FinalTestRQE/SpecFlowFeature1.feature
@@ -0,0 +1,12 @@
+﻿Feature: SpecFlowFeature1
+	In order to calculate Angles
+	As a User
+	I want to calculate Sinus Tangens and Cosinus of a number
+
+@mytag
+Scenario: Calculate Sin, Cos, Tan
+	Given the number to transform is Math.Pi
+	When the number is transformed
+	Then Sin should be 0
+	And Cos should be -1
+	And Tan should be 0
---
+++ FinalTestRQE/SpecFlowFeature1Steps.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace FinalTestRQE
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+
+        private double cos, tan, sin,pi;
+        [Given(@"the number to transform is Math\.Pi")]
+        public void GivenTheNumberToTransformIsMath_Pi()
+        {
+            pi=Math.PI;
+        }
+        
+        [When(@"the number is transformed")]
+        public void WhenTheNumberIsTransformed()
+        {
+            cos = Math.Cos(pi);
+            sin = Math.Sin(pi);
+            tan = Math.Tan(pi);
+        }
+        
+        [Then(@"Sin should be (.*)")]
+        public void ThenSinShouldBe(int p0)
+        {
+            Assert.Equal(p0,sin,5);
+        }
+        
+        [Then(@"Cos should be (.*)")]
+        public void ThenCosShouldBe(int p0)
+        {
+            Assert.Equal(p0, cos, 5);
+        }
+        
+        [Then(@"Tan should be (.*)")]
+        public void ThenTanShouldBe(int p0)
+        {
+            Assert.Equal(p0, tan, 5);
+        }
+    }
+}
